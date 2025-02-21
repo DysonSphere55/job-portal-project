7d35f8a0da7f8c4958c84b3dad88165124c69fcf
Denis Suchan <suchan.denis@gmail.com>
2025-02-21 16:47:21 +0100

    add security package & login.html

--- src/main/java/com/jobportal/jobportal/controller/UsersController.java
+++ src/main/java/com/jobportal/jobportal/controller/UsersController.java
@@ -12,6 +12,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.Date;
+
 @Controller
 public class UsersController {
 
@@ -48,6 +50,8 @@ public class UsersController {
             return "register";
         }
 
+        user.setActive(true);
+        user.setRegisteredDate(new Date());
         Users savedUser = usersService.save(user);
 
         if (user.getUsersType().getType().equals("RECRUITER")) {
